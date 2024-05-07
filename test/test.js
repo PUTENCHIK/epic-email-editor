7a77e7eee2b9515a4a6efae5826f5b5d079f1789
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-05-07 21:52:39 +0800

    Updated test files

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
     let window = new SendEmailWindow().render();
-    console.log(window);
     document.getElementsByClassName("main")[0].append(window);
+
+    let ps = document.getElementsByTagName("p");
+    for (let i = 0; i < ps.length; i++) {
+        ps[i].addEventListener("click", () => {
+            console.log(ps[i]);
+        });
+    }
 });
